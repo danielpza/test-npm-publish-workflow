43b6e06888464a18890c4ec805fc44258991876e
Daniel Perez Alvarez <danielpza@protonmail.com>
2025-09-17 21:30:28 -0700

    chore: update bump script

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -2,10 +2,14 @@
 
 version=$(pnpm git-cliff --bumped-version)
 
+if [[ $version == v* ]]; then
+  version=${version:1}
+fi
+
 pnpm git-cliff --tag "${version}" --output CHANGELOG.md
 pnpm version "${version}" --no-git-tag-version
 
 git add CHANGELOG.md package.json
 
-git commit -m "chore(release): ${version}"
-git tag -a -m "${version}" "${version}"
+git commit -m "chore(release): v${version}"
+git tag -a -m "${version}" v"${version}"
